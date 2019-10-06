b2a97ec103cbdab0b94f8834410eb790b8127d81
pantadeusz <tadeusz@puzniakowski.pl>
2019-10-06 08:55:30 +0200

    first working test

---
+++ src/test/java/pl/edu/pjatk/tau/VectorTest.java
@@ -0,0 +1,15 @@
+package pl.edu.pjatk.tau;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+
+@RunWith(JUnit4.class)
+public class VectorTest {
+    @Test
+    public void emptyTest() {
+        Assert.assertTrue(true);
+    }
+}
